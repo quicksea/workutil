ffd353746f8bc366f1ada9a030b43ed95cdc3bfd
quicksea <quicksea@gmail.com>
2016-09-18 01:21:31 -0600

    9/18 commit, add splitcol func

--- R/sxpdatafunc.R
+++ R/sxpdatafunc.R
@@ -1,5 +1,6 @@
 #' interpolate x based on y. Usually x is vdm, y is sigma, interpolate the required vdm@-1s, 0s and 1s
 #'
+
 #'take a data.table and interpolate x based y level
 #'@param dt a data.table
 #'@param x x colname string
@@ -7,8 +8,8 @@
 #'@param grp group by column string
 #'@param already_ordered if the table is already sorted
 #'@return a table.table giving lowbound highbound and fitted solution
-#'@export
 #'@import data.table
+#'@export
 interpolatex <-
   function(dt,target = c(0),x = 'x', y = 'y', grp = 'grp', already_ordered =
              FALSE) {
@@ -38,3 +39,34 @@ interpolatex <-
             (target - y_lowbound) + x_lowbound];
     return(dtcat);
   }
+
+#'take a data.table, split col into multiple columns based separator, in place change to dt
+#'mode 1: split "X:1_Y:2" into two columns X,Y, fill with 1,2;
+#'mode 0: split "1_2" into 1 and 2, column names defined by splitcolnames if given, otherwise use V1,V2...
+#'@param dt a data.table
+#'@param col column to split, no quote
+#'@param sep1 level 1 sep1
+#'@param sep2 level 1 sep2
+#'@param mode default 1
+#'@param splitcolnames used in mode 0, names of splitted columns
+#'@return no return, inplace change to dt
+#'@export
+splitcol <- function(dt, col, sep1 = '_', sep2 = ':', mode = 1, splitcolnames = NULL) {
+  grp = deparse(substitute(col))
+  if (mode == 1) {
+    str1 = dt[1,grp];
+    headerlist = as.list(unlist(strsplit(str1, sep1)));
+    namesplit <- function(x, sep) {unlist(strsplit(x,sep))[1]};
+    headers = unlist(lapply(headerlist, namesplit, sep2));
+    dt [, headers := tstrsplit(grp, sep1), with=FALSE];
+    for (header in headers) {
+      dt [, eval(header) := tstrsplit(get(header), sep2)[2]]
+    }
+  } else if (mode == 0) {
+    str1 = dt[1,grp];
+    headerlist = as.list(unlist(strsplit(str1, sep1)));
+    n = length(headerlist)
+    if (is.null(splitcolnames)) {splitcolnames=paste0(rep('V', n),1:n)}
+    dt [, splitcolnames := tstrsplit(grp, sep1), with=FALSE];
+  }
+}
